a41368981ffd3fe9caf1318b3dfd4fe1792f48e6
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-05-20 13:30:18 +0400

    Create factory function for player

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 /*
 Create player factory function
-Create arrays for each player
-Create score variable for each player
-Create name variable for each player
+Create arrays for each player x
+Create variable for which mark the player uses x
+Create score variable for each player x
+Create name variable for each player x
 Add functions for displaying and modifying variables
 
 Create module for gameboard
@@ -13,3 +14,27 @@ Add a function for checking agains winning conditions or ties
 
 Create game module which will include the gameboard module and also handle drawing the html
 */
+
+function player(name="Player", mark) {
+    let score = 0;
+    let placedMarks = new Array(9).fill(false);
+    const showInfo= function() {
+        return {name, mark, score, placedMarks};
+    };
+    const resetScore = function() {
+        score = 0;
+    }
+    const addScore = function() {
+        return ++score;
+    }
+    const setName = function(newName) {
+        name = newName;
+    };
+    const resetMarks = function() {
+        placedMarks = new Array(9).fill(false);
+    };
+    const placeMark = function(index) {
+        placedMarks[index] = true;
+        return placedMarks;
+    }
+}
