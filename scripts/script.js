254b315d3d325d9119d738914bc6fc0ef3c8e201
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-05-21 13:59:06 +0400

    Create gameloop module for handling drawing the game and fix array comparison logic

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,12 @@ Add a function for checking agains winning conditions or ties x
 Add a function for resetting the game x
 Add a function for handling game results x
 
-Create game module which will include the gameboard module and also handle drawing the html
+Create gameLoop module which will include the gameboard module and also handle drawing the html
+Create variables for the interactible divs x
+Add functions for placing marks x
+Add functions for starting, resetting and ending games x
+Add functions for choosing player names
+Add functions for hard resetting game
 */
 
 function player(name="Player", mark) {
@@ -78,14 +83,21 @@ const gameboard = (function(playerFunc) {
             return player1.showInfo().placedMarks[index] || player2.showInfo().placedMarks[index];
         });
     };
+    const compareWithWinning = function(arr) {
+        return winningBoards.some((winningArray) => {
+            return winningArray.every((value, index) => {
+                if ((value === false) || (value === true && value === arr[index])) {
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+        });
+    };
     const checkWin = function() {
-        if (winningBoards.some( (value) => {
-            return value.toString() === player1.showInfo().placedMarks.toString()
-        })) {
+        if (compareWithWinning(player1.showInfo().placedMarks)) {
             return player1.showInfo().name;
-        } else if (winningBoards.some( (value) => {
-            return value.toString() === player2.showInfo().placedMarks.toString();
-        })) {
+        } else if (compareWithWinning(player2.showInfo().placedMarks)) {
             return player2.showInfo().name;
         } else if (placedMarks.toString() === tieBoard.toString()) {
             return "Tie";
@@ -118,3 +130,65 @@ const gameboard = (function(playerFunc) {
     };
     return {player1, player2, checkWin, hardReset, softReset, handleResult, calcPlacedMarks};
 })(player);
+
+const gameLoop = (function(board) {
+    const boardDivs = document.querySelectorAll(".grid-container>div");
+    const p1NameSpan = document.querySelector(".p1-name");
+    const p1ScoreSpan = document.querySelector(".p1-score");
+    const p2NameSpan = document.querySelector(".p2-name");
+    const p2ScoreSpan = document.querySelector(".p2-score");
+    let turn = 1;
+    const start = function() {
+        boardDivs.forEach((div) => {
+            div.addEventListener("click", (e) => {
+                if (div.textContent !== ""){
+                    return;
+                }
+                if (turn === 1){
+                    board.player1.placeMark(parseInt(div.dataset.index));
+                    div.textContent = "X";
+                    turn = 2;
+                } else {
+                    board.player2.placeMark(parseInt(div.dataset.index));
+                    div.textContent = "O";
+                    turn = 1;
+                }
+                board.calcPlacedMarks();
+                handleResult();
+            })
+        });
+    };
+    const updateScores = function() {
+        p1ScoreSpan.textContent = board.player1.showInfo().score;
+        p2ScoreSpan.textContent = board.player2.showInfo().score;
+    };
+    const resetBoard = function() {
+        turn = 1;
+        boardDivs.forEach((div) => {
+            div.textContent = "";
+        });
+    };
+    const handleResult = function() {
+        const result = board.checkWin();
+        if (result !== "InProgress") {
+            board.handleResult();
+            updateScores();
+            switch (result) {
+                case "Player 1":
+                    alert("Player 1 Wins!");
+                    break;
+                case "Player 2":
+                    alert("Player 2 Wins!");
+                    break;
+                case "Tie":
+                    alert("Tie!");
+                    break;
+            }
+            resetBoard();
+        }
+
+    };
+    return {start};
+})(gameboard);
+
+gameLoop.start();
